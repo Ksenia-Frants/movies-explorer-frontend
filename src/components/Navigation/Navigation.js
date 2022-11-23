de278e8d7af7b4a4fe0636a96e3dd67eab98b2e7
Ksenia Frants <frants.ksenka@gmail.com>
2022-11-23 22:55:29 +0700

    Сделать основную работу по странице с фильмами

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Route, Link } from 'react-router-dom';
 import './Navigation.css';
 
 function Navigation() {
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+
+  function onClickMenu() {
+    setIsMenuOpened(!isMenuOpened);
+  }
+
   return (
     <nav className='navigation'>
       <Route exact path='/'>
@@ -39,6 +45,45 @@ function Navigation() {
             </Link>
           </li>
         </ul>
+        <button onClick={onClickMenu} className='navigation__burger-button'>
+          <span></span>
+        </button>
+        {isMenuOpened ? (
+          <>
+            <div className='navigation__background'></div>
+            <div className='navigation__container'>
+              <button className='navigation__cross-button' onClick={onClickMenu}>
+                &#x2716;
+              </button>
+              <ul className='navigation__menu'>
+                <li className='navigation__item'>
+                  <Link to='/' className='navigation__link navigation__link_type_menu'>
+                    Главная
+                  </Link>
+                </li>
+                <li className='navigation__item'>
+                  <Link to='/movies' className='navigation__link navigation__link_type_menu'>
+                    Фильмы
+                  </Link>
+                </li>
+                <li className='navigation__item'>
+                  <Link to='/saved-movies' className='navigation__link navigation__link_type_menu'>
+                    Сохранённые фильмы
+                  </Link>
+                </li>
+                <li className='navigation__item navigation__item_type_account'>
+                  <Link
+                    to='/saved-movies'
+                    className='navigation__link navigation__link_type_account'>
+                    Аккаунт
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </>
+        ) : (
+          ''
+        )}
       </Route>
     </nav>
   );

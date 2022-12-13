d6bdfeff3978f025e952621d9a3865f0b6e3881d
Ksenia Frants <frants.ksenka@gmail.com>
2022-12-13 13:34:08 +0700

    Исправить лейблы в профиле и ссылки в портфолио

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,5 @@
 import { React, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Profile.css';
 
 function Profile() {
@@ -14,7 +15,7 @@ function Profile() {
       <h1 className='profile__title'>Привет, Ксения!</h1>
       <form className='profile__form'>
         <fieldset className='profile__fieldset profile__fieldset_type_name'>
-          <label className='profile__label' for='name'>
+          <label className='profile__label' htmlFor='name'>
             Имя
           </label>
           <input
@@ -27,7 +28,7 @@ function Profile() {
             required></input>
         </fieldset>
         <fieldset className='profile__fieldset profile__fieldset_type_email'>
-          <label className='profile__label' for='email'>
+          <label className='profile__label' htmlFor='email'>
             E-mail
           </label>
           <input
@@ -52,7 +53,9 @@ function Profile() {
         )}
       </form>
       {disabled ? (
-        <button className='profile__button profile__button_type_out'>Выйти из аккаунта</button>
+        <Link to='/' className='profile__button profile__button_type_out'>
+          Выйти из аккаунта
+        </Link>
       ) : (
         ''
       )}
